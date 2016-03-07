50c7457570feb5eb00b24f118e1d15f3a8b9a8c5
kgryte <kgryte@gmail.com>
2016-03-07 04:31:30 -0800

    Add test fixtures

---
+++ test/fixtures/opts.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function getOpts() {
+	var opts = {
+		'repos': [
+			'math-io/erf'
+		],
+		'token': 'abcdef123!'
+	};
+	return opts;
+}
+
+
+// EXPORTS //
+
+module.exports = getOpts;
